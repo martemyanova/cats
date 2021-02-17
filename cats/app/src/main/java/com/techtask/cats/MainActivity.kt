b27059afd40a7d8014f1dacd6758f2ecd37d2eae
Julia Martemyanova <martemyanova.julia@gmail.com>
2021-02-17 22:52:31 +0000

    Attach CatsFragment to MainActivity.

--- cats/app/src/main/java/com/techtask/cats/MainActivity.kt
+++ cats/app/src/main/java/com/techtask/cats/MainActivity.kt
@@ -1,36 +1,21 @@
 package com.techtask.cats
 
 import android.os.Bundle
-import android.widget.TextView
-import androidx.lifecycle.lifecycleScope
 import com.techtask.cats.common.BaseActivity
-import com.techtask.cats.domain.usecase.FetchCatsUseCase
-import kotlinx.coroutines.launch
-import javax.inject.Inject
+import com.techtask.cats.presentation.CatsFragment
 
 class MainActivity : BaseActivity() {
-
-    @Inject
-    lateinit var fetchCatsUseCase: FetchCatsUseCase
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        injector.inject(this)
-
-        lifecycleScope.launch {
-            val stringBuilder = StringBuilder("Resuls:/n")
-            val result = fetchCatsUseCase.execute()
-            if (result is FetchCatsUseCase.Result.Success) {
-                result.cats.forEach {
-                    stringBuilder.append(it.name)
-                    stringBuilder.append("/n")
-                }
-            }
-
-            findViewById<TextView>(R.id.content).text = stringBuilder.toString()
+        if (supportFragmentManager.findFragmentByTag(CatsFragment.TAG) == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.content,
+                    CatsFragment.newInstance(),
+                    CatsFragment.TAG)
+                .commit()
         }
-
     }
 }
