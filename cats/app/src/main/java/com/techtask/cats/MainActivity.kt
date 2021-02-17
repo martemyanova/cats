a32ed4a6180c7fc327378aae3098241810320249
Julia Martemyanova <martemyanova.julia@gmail.com>
2021-02-17 12:41:24 +0000

    Show first result on the main screen.

--- cats/app/src/main/java/com/techtask/cats/MainActivity.kt
+++ cats/app/src/main/java/com/techtask/cats/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.techtask.cats
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.lifecycle.lifecycleScope
+import com.techtask.cats.common.BaseActivity
+import com.techtask.cats.domain.usecase.FetchCatsUseCase
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
+
+    @Inject
+    lateinit var fetchCatsUseCase: FetchCatsUseCase
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        injector.inject(this)
+
+        lifecycleScope.launch {
+            val stringBuilder = StringBuilder("Resuls:/n")
+            val result = fetchCatsUseCase.execute()
+            if (result is FetchCatsUseCase.Result.Success) {
+                result.cats.forEach {
+                    stringBuilder.append(it.name)
+                    stringBuilder.append("/n")
+                }
+            }
+
+            findViewById<TextView>(R.id.content).text = stringBuilder.toString()
+        }
+
     }
 }
