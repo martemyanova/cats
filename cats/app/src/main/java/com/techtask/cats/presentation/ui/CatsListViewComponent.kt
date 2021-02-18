ceaead6d677c98c614d17273a4f0f98f33613fa4
Julia Martemyanova <martemyanova.julia@gmail.com>
2021-02-18 14:16:58 +0000

    Show progress bar while data is being loaded.

--- cats/app/src/main/java/com/techtask/cats/presentation/ui/CatsListViewComponent.kt
+++ cats/app/src/main/java/com/techtask/cats/presentation/ui/CatsListViewComponent.kt
@@ -3,7 +3,9 @@ package com.techtask.cats.presentation.ui
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
 import androidx.annotation.IdRes
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.techtask.cats.R
@@ -31,16 +33,29 @@ class CatsListViewComponent @Inject constructor() {
             catsListAdapter = CatsListAdapter()
             adapter = catsListAdapter
         }
+        showLoadingProgress()
     }
 
     fun bindData(cats: List<Cat>) {
         catsListAdapter.submitList(cats)
+        hideLoadingProgress()
+    }
+
+    private fun showLoadingProgress() {
+        contentRecyclerView.isVisible = false
+        progressBarView.isVisible = true
+    }
+
+    private fun hideLoadingProgress() {
+        contentRecyclerView.isVisible = true
+        progressBarView.isVisible = false
     }
 
     private val context by lazy { rootView.context }
     private val numberOfColumns by lazy { context.resources.getInteger(R.integer.number_of_columns) }
 
     private val contentRecyclerView by lazy { rootView.findViewById<RecyclerView>(R.id.rv_content) }
+    private val progressBarView by lazy { rootView.findViewById<ProgressBar>(R.id.pb_progress_bar) }
 
     private inline fun <reified T : View> findViewById(@IdRes widgetResId: Int): T {
         return rootView.findViewById(widgetResId)
